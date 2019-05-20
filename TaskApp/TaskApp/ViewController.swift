3a1b80aafcbba0427567d5b86c4950f3935ad0bc
Madeleine <madeleine.gillard@gmail.com>
2019-05-20 18:10:28 +1000

    initial commit

---
+++ TaskApp/TaskApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskApp
+//
+//  Created by Madeleine Gillard on 20/5/19.
+//  Copyright © 2019 Madeleine Gillard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TaskApp/TaskAppTests/TaskAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  TaskAppTests.swift
+//  TaskAppTests
+//
+//  Created by Madeleine Gillard on 20/5/19.
+//  Copyright © 2019 Madeleine Gillard. All rights reserved.
+//
+
+import XCTest
+@testable import TaskApp
+
+class TaskAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
