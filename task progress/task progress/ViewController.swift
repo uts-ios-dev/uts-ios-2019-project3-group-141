e364dcc58ef5f200b85ce20c6bf8afe4a7cab718
jean <jean@jeans-MacBook-Pro.local>
2019-05-20 18:45:35 +1000

    init

---
+++ task progress/task progress/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  task progress
+//
+//  Created by jean on 16/5/2562 BE.
+//  Copyright © 2562 uts13020449. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ task progress/task progressTests/task_progressTests.swift
@@ -0,0 +1,34 @@
+//
+//  task_progressTests.swift
+//  task progressTests
+//
+//  Created by jean on 16/5/2562 BE.
+//  Copyright © 2562 uts13020449. All rights reserved.
+//
+
+import XCTest
+@testable import task_progress
+
+class task_progressTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
