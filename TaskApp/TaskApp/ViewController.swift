3d8b2c05ceb76d9fe467588f2ac11e860aa08694
Madeleine <madeleine.gillard@gmail.com>
2019-05-30 22:28:09 +1000

    login functionality works properly

--- TaskApp/TaskApp/ViewController.swift
+++ TaskApp/TaskApp/ViewController.swift
@@ -12,55 +12,62 @@ import Firebase
 
 
 class ViewController: UIViewController {
+    var allowlogin:Bool
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    required init?(coder aDecoder: NSCoder) {
+        self.allowlogin = false
+        super.init(coder: aDecoder)
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+         //1
+        Auth.auth().addStateDidChangeListener() { auth, user in
+            // 2
+            if user != nil {
+                print("LOG: authenticated ")
+                // 3
+                self.allowlogin = true
+                self.performSegue(withIdentifier: "wholeScene", sender: nil)
+            }
+            else {
+                self.allowlogin = false
+            }
+        }
+    }
+    
+    @IBOutlet weak var loginBtn: UIButton!
     @IBOutlet weak var usernameTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
     
-    @IBAction func signUpClick(_ sender: Any) {
-        signup()
-    }
-    @IBAction func changetoTask(_ sender: Any) {
+    @IBAction func loginClicked(_ sender: UIButton) {
         login()
-//         performSegue( withIdentifier: "segueTask", sender: self)
+    }
+    
+    @IBAction func signupClicked(_ sender: UIButton) {
+        signup()
     }
     
     func login(){
-        var errorBool = false
         if self.usernameTF.text == "" || self.passwordTF.text == "" {
-            print("empty box")
-            let alert = UIAlertController(title: "Error", message: "Please enter an email and password", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .cancel, handler: { _ in
-                NSLog("The \"no email or password\" alert occured.")
-            }))
-            self.present(alert, animated: true, completion: nil)
-            
+            print("LOG: empty box")
         } else {
+            print("LOG: running the signin else part 1")
+            //this is running too long.
             Auth.auth().signIn(withEmail: self.usernameTF.text!, password: self.passwordTF.text!) { (user, error) in
                 if error == nil {
-                    //Print into the console if successfully logged in
-                    print("You have successfully logged in")
-                    
-//                    //Go to the HomeViewController if the login is sucessful
-//                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
-//                    self.present(vc!, animated: true, completion: nil)
-                    
+                    print("LOG:You have successfully logged in")
                 } else {
-                    errorBool = true
-                    print("error with login")
-                    print(error?.localizedDescription)
+                    print("LOG: error with login")
+                    print(error!.localizedDescription)
                 }
             }
-        }
-        if errorBool {
-            let errAlert = UIAlertController(title: "Login Error", message: "Incorrect Creds", preferredStyle: .alert)
-            errAlert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-            self.present(errAlert, animated: true, completion: nil)
-            errAlert.show(self, sender: self)
         }
     }
     
@@ -68,30 +75,24 @@ class ViewController: UIViewController {
         if usernameTF.text == "" {
             print("username is empty")
             let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
-            
             let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
             alertController.addAction(defaultAction)
-            
             present(alertController, animated: true, completion: nil)
-            
         } else {
             Auth.auth().createUser(withEmail: usernameTF.text!, password: passwordTF.text!) { (user, error) in
                 
                 if error == nil {
                     print("You have successfully signed up")
-                    
+                    Auth.auth().signIn(withEmail: self.usernameTF.text!, password: self.passwordTF.text!)
                 } else {
                     print("error in signing up")
                     print(error!)
                     let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
-                    
                     let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
                     alertController.addAction(defaultAction)
-                    
                     self.present(alertController, animated: true, completion: nil)
                 }
             }
         }
-        
     }
 }
